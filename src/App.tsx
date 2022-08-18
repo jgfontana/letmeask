8b13d77c9d875a6503fce9530025928a86a2b07e
Joao Fontana <jgfontana98@gmail.com>
2022-08-18 16:08:04 -0300

    onAuthStateChanged

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import {
   Routes,
   Route 
 } from "react-router-dom";
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { firebase, auth } from "./services/firebase";
 
 type User = {
@@ -23,6 +23,24 @@ export const AuthContext = createContext({} as AuthContextType);
 
 function App() {
   const [user, setUser] = useState<User>();
+
+  useEffect(() => {
+    auth.onAuthStateChanged(user => {
+      if (user) {
+        const { displayName, photoURL, uid } = user;
+
+        if (!displayName || !photoURL) {
+          throw new Error("Missing information from Google Account.");
+        }
+
+        setUser({
+          id: uid,
+          name: displayName,
+          avatar: photoURL
+        })
+      }
+    })
+  }, [])
 
   async function signInWithGoogle() {
     const provider = new firebase.auth.GoogleAuthProvider();
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ import googleIconImg from '../assets/images/google-icon.svg'
 import { Button } from '../components/Button';
 
 import '../styles/auth.scss'
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { AuthContext } from '../App';
 
 export function Home() {
