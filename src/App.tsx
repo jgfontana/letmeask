c791a55ead0ce1bb9bbc2f59cd28534da502d557
Joao Fontana <jgfontana98@gmail.com>
2022-08-18 16:03:04 -0300

    get user data

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,57 @@
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
-
 import { 
   BrowserRouter as Router,
   Routes,
   Route 
 } from "react-router-dom";
+import { createContext, useState } from "react";
+import { firebase, auth } from "./services/firebase";
+
+type User = {
+  id: string,
+  name: string,
+  avatar: string
+}
+
+type AuthContextType = {
+  user: User | undefined;
+  signInWithGoogle: () => Promise<void>;
+}
+
+export const AuthContext = createContext({} as AuthContextType);
 
 function App() {
+  const [user, setUser] = useState<User>();
+
+  async function signInWithGoogle() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    const result = await auth.signInWithPopup(provider);
+
+    if (result.user) {
+      const { displayName, photoURL, uid } = result.user;
+
+      if (!displayName || !photoURL) {
+        throw new Error("Missing information from Google Account.");
+      }
+
+      setUser({
+        id: uid,
+        name: displayName,
+        avatar: photoURL
+      })
+    }
+  }
+
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/rooms/new" element={<NewRoom />} />
-      </Routes>
+      <AuthContext.Provider value={{ user, signInWithGoogle }}>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/rooms/new" element={<NewRoom />} />
+        </Routes>
+      </AuthContext.Provider>
     </Router>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,5 @@
 import { useNavigate } from 'react-router-dom'
 
-import { auth, firebase } from '../services/firebase'
-
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
@@ -9,18 +7,19 @@ import googleIconImg from '../assets/images/google-icon.svg'
 import { Button } from '../components/Button';
 
 import '../styles/auth.scss'
+import { useContext } from 'react';
+import { AuthContext } from '../App';
 
 export function Home() {
   const navigate = useNavigate();
 
-  function handleCreateRoom() {
-    const provider = new firebase.auth.GoogleAuthProvider();
+  const { user, signInWithGoogle } = useContext(AuthContext)
 
-    auth.signInWithPopup(provider).then(result => {
-      console.log(result);
-      navigate("/rooms/new", { replace: true });
-    });
-
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle()
+    }
+    navigate("/rooms/new", { replace: true });
   }
 
   return (
