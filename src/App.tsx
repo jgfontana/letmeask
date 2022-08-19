00cef1a4698ad027e62dd00e83ef82af90475f21
Joao Fontana <jgfontana98@gmail.com>
2022-08-19 10:14:20 -0300

    like + remove question + end room

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 import { AuthContextProvider } from './contexts/AuthContext'
 import { Room } from "./pages/Room";
+import { AdminRoom } from "./pages/AdminRoom";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/rooms/new" element={<NewRoom />} />
             <Route path="/rooms/:id" element={<Room />} />
+            <Route path="/admin/rooms/:id" element={<AdminRoom />} />
         </Routes>
       </AuthContextProvider>   
     </Router>
--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -1,5 +1,19 @@
 import { useEffect, useState } from "react";
 import { database } from "../services/firebase";
+import { useAuth } from "./useAuth";
+
+type FirebaseQuestions = Record<string, {
+  author: {
+    name: string;
+    avatar: string;
+  }
+  content: string;
+  isAnswered: boolean;
+  isHighLighted: boolean;
+  likes: Record<string, {
+    authorId: string;
+  }>
+}> 
 
 type QuestionType = {
   id: string;
@@ -10,19 +24,12 @@ type QuestionType = {
   content: string;
   isAnswered: boolean;
   isHighLighted: boolean;
+  likeCount: number;
+  likeId: string | undefined;
 }
 
-type FirebaseQuestions = Record<string, {
-  author: {
-    name: string;
-    avatar: string;
-  }
-  content: string;
-  isAnswered: boolean;
-  isHighLighted: boolean;
-}> 
-
 export function useRoom(roomId: string) {
+  const { user } = useAuth();
   const [questions, setQuestions] = useState<QuestionType[]>([]);
   const [title, setTitle] = useState('');
 
@@ -39,14 +46,20 @@ export function useRoom(roomId: string) {
           content: value.content,
           author: value.author,
           isHighLighted: value.isHighLighted,
-          isAnswered: value.isAnswered
+          isAnswered: value.isAnswered,
+          likeCount: Object.values(value.likes ?? {}).length,
+          likeId: Object.entries(value.likes ?? {}).find(([key, like]) => like.authorId === user?.id)?.[0],
         }
       })
 
       setTitle(databaseRoom.title);
       setQuestions(parsedQuestions);
     })
-  }, [roomId]);
+
+    return () => {
+      roomRef.off('value');
+    }
+  }, [roomId, user?.id]);
 
   return { questions, title }
 }
